5ec56968c133bf84129f33376dbaefc8814b6fbf
HoangMinhNg <ngminh3004@gmail.com>
2024-01-22 10:11:01 +0700

    Add project files.

---
+++ PRN221ProjectGroup/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRN221ProjectGroup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
