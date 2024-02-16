e8173bd83901ea38df30584c4f6c5f6e3bd15c86
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-02-16 16:58:54 +0700

    Fix model, add first migration

--- BusinessObject/User.cs
+++ BusinessObject/User.cs
@@ -6,13 +6,13 @@
         public string UserName { get; set; }
         public string UserPassword { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string KnownAs { get; set; }
+        public string? KnownAs { get; set; }
         public DateTime CreateAt { get; set; }
         public DateTime LastActive { get; set; }
-        public string Gender { get; set; }
+        public string? Gender { get; set; }
         public string? Introduction { get; set; }
-        public string Interest { get; set; }
-        public string City { get; set; }
+        public string? Interest { get; set; }
+        public string? City { get; set; }
 
         public List<Photo> photos { get; set; } = new();
         public List<Friend> SentByUsers { get; set; }
--- DataAccessLayer/Data/DataContext.cs
+++ DataAccessLayer/Data/DataContext.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
-using BusinessObject;
+﻿using BusinessObject;
+using Microsoft.EntityFrameworkCore;
 
-namespace DataAccessLayer
+namespace PRN221ProjectGroup.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext() { }
+        public DataContext()
+        {
+        }
+
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
@@ -16,15 +19,6 @@ namespace DataAccessLayer
         public DbSet<Message> Messages { get; set; }
         public DbSet<Participants> Participants { get; set; }
         public DbSet<Photo> Photos { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=12345;database=ChatSystem;\nTrustServerCertificate=True");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
