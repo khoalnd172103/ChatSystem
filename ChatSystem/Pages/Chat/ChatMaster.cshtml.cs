65506b3f761c20997e8c0d0dd15d33150a6476d7
AskWhyNot <109494300+askwhynot1309@users.noreply.github.com>
2024-02-26 23:12:33 +0700

    add dislay group name

--- ChatSystem/Pages/Chat/ChatMaster.cshtml.cs
+++ ChatSystem/Pages/Chat/ChatMaster.cshtml.cs
@@ -9,17 +9,23 @@ namespace ChatSystem.Pages.Chat
     public class ChatMasterModel : PageModel
     {
         private readonly IUserRepository _userRepository;
+        private readonly IConversationRepository _conversationRepository;
 
-        public List<User> GroupChatParticipants { get; private set; }
+        public List<User> GroupChatParticipants { get; set; }
+        public Conversation Conversation { get; set; }
 
-        public ChatMasterModel(IUserRepository userRepository)
+        public ChatMasterModel(IUserRepository userRepository, IConversationRepository conversationRepository)
         {
             _userRepository = userRepository;
+            _conversationRepository = conversationRepository;
         }
         public void OnGet()
         {
             //hard code for test
-            GroupChatParticipants = _userRepository.GetUserInGroupChat(1);
+            int conversationId = 1;
+            GroupChatParticipants = _userRepository.GetUserInGroupChat(conversationId);
+            Conversation = _conversationRepository.GetConversationById(conversationId);
+            
         }
     }
 }
--- Repository/IConversationRepository.cs
+++ Repository/IConversationRepository.cs
@@ -5,5 +5,7 @@ namespace Repository
     public interface IConversationRepository : IBaseRepository<Conversation>
     {
         public void CreateGroup(int creatorId, string groupName, List<string> memberIdList);
+
+        public Conversation GetConversationById(int conversationId);
     }
 }
