07c5d9c728052851333cb64d07c28ef6f470d26a
Nguyen Van Hoang Minh <Ngminh3004@gmail.com>
2024-03-15 14:52:53 +0700

    merge all code into main

--- ChatSystem/Pages/Users/UserProfile.cshtml.cs
+++ ChatSystem/Pages/Users/UserProfile.cshtml.cs
@@ -14,12 +14,12 @@ namespace ChatSystem.Pages.Users
         private readonly IFriendRepository friendRepository;
         private readonly IConversationRepository _conversationRepository;
 
-        public UserProfileModel(IUserRepository userRepository, IFriendRepository friendRepository, IHubContext<NotificationHub> notificationContext)
+        public UserProfileModel(IUserRepository userRepository, IFriendRepository friendRepository, IHubContext<NotificationHub> notificationContext, IConversationRepository conversationRepository)
         {
             _userRepository = userRepository;
             this.friendRepository = friendRepository;
             this.notificationContext = notificationContext;
-            _conversationRepository = conversationRepository;
+            this._conversationRepository = conversationRepository;
         }
 
         public UserProfileDto UserProfile { get; set; }
@@ -30,9 +30,6 @@ namespace ChatSystem.Pages.Users
 
         public int FriendStatus { get; set; } = 0;
 
-        [BindProperty]
-        public int UserId { get; set; }
-
         public IActionResult OnGet(int UserId)
         {
             var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
@@ -41,7 +38,7 @@ namespace ChatSystem.Pages.Users
                 IsLogined = true;
                 IsFriend = friendRepository.CheckIsFriendAsync(int.Parse(idClaim.Value), UserId);
             }
-            //int loginUserId = int.Parse(idClaim.Value);
+            int loginUserId = int.Parse(idClaim.Value);
 
             var user = _userRepository.GetUserWithPhoto(UserId);
             if (user == null)
@@ -73,98 +70,99 @@ namespace ChatSystem.Pages.Users
 
             return Page();
         }
-        
-      public IActionResult OnPostStartConversationAsync()
-      {
-        var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
-        if (idClaim != null)
+
+        public IActionResult OnPostStartConversationAsync()
         {
-        IsLogined = true;
-        }
-    int loginUserId = int.Parse(idClaim.Value);
+            var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
+            if (idClaim != null)
+            {
+                IsLogined = true;
+            }
+            int loginUserId = int.Parse(idClaim.Value);
 
-    var conversation = _conversationRepository.GetConversationBySenderIdAndReceiverId(loginUserId, UserId);
+            var conversation = _conversationRepository.GetConversationBySenderIdAndReceiverId(loginUserId, UserId);
 
-    if (conversation != null)
-    {
-        return RedirectToPage("/Chat/ChatMaster", new { id = conversation.ConversationId });
-    }
-    else
-    {
-        var sender = _userRepository.GetUser(loginUserId);
-        var receiver = _userRepository.GetUser(UserId);
+            if (conversation != null)
+            {
+                return RedirectToPage("/Chat/ChatMaster", new { id = conversation.ConversationId });
+            }
+            else
+            {
+                var sender = _userRepository.GetUser(loginUserId);
+                var receiver = _userRepository.GetUser(UserId);
 
-        var newConversation = new Conversation
-        {
-            ConversationName = "Conversation of " + sender.UserName + " and " + receiver.UserName,
-            UserId = loginUserId,
-            CreateAt = DateTime.Now,
-            isGroup = false,
-            MessagesReceived = new List<Message>(),
-            Participants = new List<Participants>
+                var newConversation = new Conversation
+                {
+                    ConversationName = "Conversation of " + sender.UserName + " and " + receiver.UserName,
+                    UserId = loginUserId,
+                    CreateAt = DateTime.Now,
+                    isGroup = false,
+                    MessagesReceived = new List<Message>(),
+                    Participants = new List<Participants>
         {
             new Participants { UserId = loginUserId, isAdmin = true, status = 1 },
             new Participants { UserId = UserId, isAdmin = false, status = 1 }
         }
-        };
+                };
 
-        _conversationRepository.Create(newConversation);
+                _conversationRepository.Create(newConversation);
 
-        return RedirectToPage("/Chat/ChatMaster", new { id = newConversation.ConversationId });
+                return RedirectToPage("/Chat/ChatMaster", new { id = newConversation.ConversationId });
+            }
+        }
+
+        public async Task<IActionResult> OnPostAddFriend()
+        {
+            await notificationContext.Clients.All.SendAsync("OnSendFriendRequest");
+
+            var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
+            if (idClaim != null)
+            {
+                IsLogined = true;
+            }
+            HttpContext.Session.SetInt32("UserId", UserId);
+            int currentUserId = int.Parse(idClaim.Value);
+            var recipientUserName = _userRepository.GetUser(UserId).UserName;
+            var senderUserName = _userRepository.GetUser(currentUserId).UserName;
+            await friendRepository.SendFriendRequest(currentUserId, UserId, senderUserName, recipientUserName);
+            return RedirectToPage("/Users/UserProfile", new { userId = UserId });
+        }
+
+        public async Task<IActionResult> OnPostAcceptFriend()
+        {
+            var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
+            if (idClaim != null)
+            {
+                IsLogined = true;
+            }
+            int loginUserId = int.Parse(idClaim.Value);
+            await friendRepository.AcceptFriendRequest(UserId, loginUserId);
+            return RedirectToPage("/Users/UserProfile", new { userId = UserId });
+        }
+
+        public async Task<IActionResult> OnPostDeclineFriend()
+        {
+            var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
+            if (idClaim != null)
+            {
+                IsLogined = true;
+            }
+            int loginUserId = int.Parse(idClaim.Value);
+            await friendRepository.DeclineFriendRequest(UserId, loginUserId);
+            return RedirectToPage("/Users/UserProfile", new { userId = UserId });
+        }
+
+        public async Task<IActionResult> OnPostUnfriend()
+        {
+            var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
+            if (idClaim != null)
+            {
+                IsLogined = true;
+            }
+            int loginUserId = int.Parse(idClaim.Value);
+            //await friendRepository.DeclineFriendRequest(UserId, loginUserId);
+            return RedirectToPage("/Users/UserProfile", new { userId = UserId });
+        }
+
     }
-}
-
-public async Task<IActionResult> OnPostAddFriend()
-{
-    await notificationContext.Clients.All.SendAsync("OnSendFriendRequest");
-
-    var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
-    if (idClaim != null)
-    {
-        IsLogined = true;
-    }
-    HttpContext.Session.SetInt32("UserId", UserId);
-    int currentUserId = int.Parse(idClaim.Value);
-    var recipientUserName = _userRepository.GetUser(UserId).UserName;
-    var senderUserName = _userRepository.GetUser(currentUserId).UserName;
-    await friendRepository.SendFriendRequest(currentUserId, UserId, senderUserName, recipientUserName);
-    return RedirectToPage("/Users/UserProfile", new { userId = UserId });
-}
-
-public async Task<IActionResult> OnPostAcceptFriend()
-{
-    var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
-    if (idClaim != null)
-    {
-        IsLogined = true;
-    }
-    int loginUserId = int.Parse(idClaim.Value);
-    await friendRepository.AcceptFriendRequest(UserId, loginUserId);
-    return RedirectToPage("/Users/UserProfile", new { userId = UserId });
-}
-
-public async Task<IActionResult> OnPostDeclineFriend()
-{
-    var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
-    if (idClaim != null)
-    {
-        IsLogined = true;
-    }
-    int loginUserId = int.Parse(idClaim.Value);
-    await friendRepository.DeclineFriendRequest(UserId, loginUserId);
-    return RedirectToPage("/Users/UserProfile", new { userId = UserId });
-}
-
-public async Task<IActionResult> OnPostUnfriend()
-{
-    var idClaim = User.Claims.FirstOrDefault(claims => claims.Type == "UserId", null);
-    if (idClaim != null)
-    {
-        IsLogined = true;
-    }
-    int loginUserId = int.Parse(idClaim.Value);
-    //await friendRepository.DeclineFriendRequest(UserId, loginUserId);
-    return RedirectToPage("/Users/UserProfile", new { userId = UserId });
-}
-
 }
