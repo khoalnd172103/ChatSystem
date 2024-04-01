5b1f278322b333a56beb3c332311cd77df77393f
khoalnd <khoaly090141@gmail.com>
2024-04-01 21:24:53 +0700

    add validate when register new account

---
+++ BusinessObject/UserNameValidation.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObject
+{
+	public class UserNameValidation : ValidationAttribute
+	{
+		public override bool IsValid(object? value)
+		{
+			if (value == null)
+			{
+				return false;
+			}
+			else
+			{
+				string userName = value.ToString();
+				if (userName.Contains(' '))
+				{
+					ErrorMessage = "Username cannot contain whitespace.";
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
